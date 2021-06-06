b7d72a218d7a3ef4e708a9c1cbfe37b850355a36
Anju1415 <anjuannu1415@gmail.com>
2021-06-06 10:56:50 +0530

    data class

--- app/src/main/java/com/example/android/lifequotes/SignUpActivity.kt
+++ app/src/main/java/com/example/android/lifequotes/SignUpActivity.kt
@@ -1,7 +1,5 @@
 package com.example.android.lifequotes;
 
-import android.app.Activity
-import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
